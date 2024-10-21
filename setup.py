3c97619b01debd177d1f300c4f0fa2c66ca07972
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-10-21 09:35:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ classifiers = [
   'Intended Audience :: Game Developers',
   'Operating System :: Microsoft :: Windows :: Windows 10 :: Linux',
   'License :: Bspace License',
-  'Programming Language :: Python :: 3.0'
+  'Programming Language :: Python :: >=3.13'
 ]
  
 setup(
   name='pytgm',
-  version='2.1.2',
+  version='2.1.3',
   description='Terminal Game Maker',
   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-  url='https://youtube.tokynblast.space/programming/libraries/pytgm/init',  
+  url='https://pytgm.tokynblast.space/init',  
   author='Tokyn Blast',
   author_email='tokynblast@gmail.com',
   license='Bspace', 
