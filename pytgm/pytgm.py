fb30c4b7284b644c8c5dd90d7be0cd04476807bd
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-10 05:29:48 -0800

    Changed how random seed worked

--- pytgm/pytgm.py
+++ pytgm/pytgm.py
@@ -16,9 +16,9 @@ class random:
         @staticmethod
         def integer(min_value, max_value, seed=None):
             if seed == None:
-                try: time()
-                except: from time import time
-                seed = int(time() * 1000)
+                try: seed = int(time() * 1000)
+                except: from time import time; seed = int(time() * 1000)
+                
 
             number = (seed % (max_value - min_value + 1)) + min_value
 
