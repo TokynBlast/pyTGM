976c41b8c6f119224d6d6a4cba19141ccd8e7ff1
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-11 15:58:02 -0800

    Removed getch for relocation and rename

--- pytgm/pytgm.py
+++ pytgm/pytgm.py
@@ -168,34 +168,6 @@ class sound:
             wf.setframerate(sample_rate)
             wf.writeframes(bytearray(samples))
 
-def getch(times=1):
-    try:
-        from msvcrt import getch as g
-        for i in range(times):
-            if g == 'b\xe0': return 'ArrowDown'
-            else: return g()
-
-    except:
-        from sys import stdin
-        from tty import setraw
-        from termios import tcsetattr, TCSADRAIN, tcgetattr
-        for i in range(times):
-            fd = stdin.fileno()
-            old = tcgetattr(fd)
-            try:
-                setraw(fd)
-                c = stdin.read(1)
-                if c == '\x1b':
-                    c = stdin.read(2)
-                    if c == '[A': return 'ArrowUp'
-                    elif c == '[B': return 'ArrowDown'
-                    elif c == '[C': return 'ArrowRight'
-                    elif c == '[D': return 'ArrowLeft'
-                    else: return c
-                else:
-                    if c == ' ': return 'space'
-                    else: return c
-            finally: tcsetattr(fd, TCSADRAIN, old)
 
 class Board:
     boards = []
