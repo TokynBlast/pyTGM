01eb9962b43d32d351b40bb88421a814aa02c5a6
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-11 16:18:55 -0800

    Took out links and __all__ for relocation
    I'm putting them in __init__.py

--- pytgm/pytgm.py
+++ pytgm/pytgm.py
@@ -1,16 +1,7 @@
-__all__ = ['random','random.num','random.num.integer','random.num.binary','random.choose','random.seq','random.seq.modify','random.seq.modify.shuffle','random.seq.modify.duplicate','random.seq.modify.remove','file','file.readLine','file.modLine','graphics','graphics.cls','graphics.color','graphics.res','graphics.markup.bold','graphics.markup.italic','graphics.markup.underline','getch','sound','sound.file','sound.frequency','sound.frequency_big','Board','Board.boards','Board.add','Board.remove','Board.modify','LocalServer','b64','b64.table.table_','b64.table.tableGen','b64.table.tableSet','b64.encode','b64.decode']
-__url__ = 'https://github.com/TokynBlast/pyTGM'
-__homepage__ = 'https://pytgm.tokynblast.space/home'
-__download_url__ = 'https://pypi.org/tokynblast'
-__docs_url__ = 'https://pytgm.tokynblast.space/documentation/use'
-__bug_tracker_url__ = 'https://github.com/TokynBlast/pyTGM/issues'
-__source_code_url__ = 'https://github.com/TokynBlast/pyTGM/tree/main'
-__changelog_url__ = 'https://github.com/TokynBlast/pyTGM/blob/main/CHANGELOG.txt'
 
 # Started: 3/15/2024
 
 import os
-
 
 class b64:
   class table:
