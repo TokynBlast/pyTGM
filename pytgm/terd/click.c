82bfc629771ab8e8b7b8e3f3e43f10f92f220cb5
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-12 08:55:56 -0800

    Removed ncruses dependancy

--- pytgm/terd/click.c
+++ pytgm/terd/click.c
@@ -1,19 +1,49 @@
-#include <ncurses.h>
+#include <Python.h>
 #include <tuple>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/input.h>
 
 std::tuple<int, int> getMouseClick() {
-    initscr();
-    noecho();
-    cbreak();
-    keypad(stdscr, TRUE);
-
-    MEVENT event;
-
-    if (getmouse(&event) == OK) {
-        endwin();
-        return std::make_tuple(event.x, event.y);
+    int fd = open("/dev/input/event0", O_RDONLY | O_NONBLOCK);
+    if (fd < 0) {
+        return std::make_tuple(-1, -1);
     }
 
-    endwin();
-    return std::make_tuple(-1, -1);
+    struct input_event event;
+    while (true) {
+        ssize_t bytes = read(fd, &event, sizeof(event));
+        if (bytes < sizeof(event)) continue;
+
+        if (event.type == EV_ABS && event.code == ABS_X) {
+            close(fd);
+            return std::make_tuple(event.value, event.code);
+        }
+    }
+
+    close(fd);
+    return std::make_tuple(-1, -1);  // No click detected
+}
+
+static PyObject* py_getMouseClick(PyObject* self, PyObject* args) {
+    auto result = getMouseClick();
+    return Py_BuildValue("(ii)", std::get<0>(result), std::get<1>(result));
+}
+
+static PyMethodDef methods[] = {
+    {"getMouseClick", py_getMouseClick, METH_NOARGS, "Detect mouse click"},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef module = {
+    PyModuleDef_HEAD_INIT,
+    "click",       // Module name
+    NULL,           // Documentation
+    -1,             // Size of per-interpreter state of the module
+    methods         // Method definitions
+};
+
+PyMODINIT_FUNC PyInit_click(void) {
+    return PyModule_Create(&module);
 }
