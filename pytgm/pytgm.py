19493de9c8fb42a4d452f3b9c0428da9f9f3679d
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-11 22:28:42 -0800

    Removed graphics for relocation

--- pytgm/pytgm.py
+++ pytgm/pytgm.py
@@ -75,27 +75,6 @@ class file:
         with open('code', 'w') as code:
             code.writelines(lines)
 
-
-class graphics:
-    @staticmethod
-    def cls():
-        if os.name == 'nt':
-            os.system('cls')
-        elif os.name == 'posix':
-            os.system('clear')
-        else:
-            print('\033[H\033[J', end='')
-            
-    @staticmethod
-    def color(r,g,b): return f"\x1b[38;2;{r};{g};{b}m"
-                
-    res = "\x1b[0m"
-
-    class markup:
-        bold = '\x1b[1m'
-        italic = '\x1b[3m'
-        underline = '\x1b[4m'
-
 def LocalServer(PORT_, To_send):
     import socket
     import threading
