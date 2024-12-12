0915bca67719b6c811583e52d2c4c6efe5777233
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-12 09:38:03 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,45 @@
 from setuptools import setup, find_packages, Extensions
- 
+from sys import platform 
 classifiers = [
   'Development Status :: 5 - Production/Stable',
   'Intended Audience :: Game Developers',
-  'Operating System :: Microsoft :: Windows :: Windows 10 :: Linux',
+  'Operating System :: Microsoft :: Windows',
+  'Operating System :: POSIX :: Linux',
+  'Operating System :: MacOS',
   'License :: Bspace License',
-  'Programming Language :: Python :: >=3.13'
+  'Programming Language :: Python :: 3',
+  'Programming Language :: Python :: 3.13',
 ]
- 
+
+extra_compile_args = {
+    'linux': ['-std=c++11', '-O3', '-Wall', '-fPIC'],
+    'darwin': ['-std=c++11', '-O3', '-Wall', '-fPIC'],
+    'win32': ['/std:c++11', '/O2', '/W4']
+}
+
+maintain_auth = 'Tokyn Blast'
+maintain_auth_contact = 'tokynblast@gmail.com'
+
 setup(
   name='pyTGM',
   version='4.0.0',
   description='Terminal Game Maker',
   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
   url='https://youtube.tokynblast.space/programming/libraries/pytgm/init',  
-  author='Tokyn Blast',
-  author_email='tokynblast@gmail.com',
-  maintainer='Tokyn Blast'
-  maintainer_email='tokynblast@gmail.com'
+  author=maintain_auth,
+  author_email=maintain_auth_contact,
+  maintainer=maintain_auth,
+  maintainer_email=maintain_auth_contact,
   license='Bspace', 
   classifiers=classifiers,
   keywords='game,game maker,terminal,tools', 
   packages=find_packages(),
   install_requires=[''] 
-  ext_modules=[Extension("click",["click.c"])]
-  puthon_requires=">=3.13"
+  ext_modules=[ Extension(
+            'click',
+            sources=['terd/click.c'],
+            extra_compile_args=extra_compile_args[sys.platform],
+        )]
+  python_requires=">=3.13"
   platforms=["Windows", "Linux", "MacOS"]
 )
