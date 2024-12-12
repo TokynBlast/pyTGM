6170d045c521a6114ffe33d0e8270946cfef5a52
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-12 00:47:51 -0800

    Create file.py

---
+++ pytgm/file.py
@@ -0,0 +1,16 @@
+def readLine(name, line=0):
+  x = open(name, 'r')
+  x.readlines()[line]
+  return x
+        
+    
+def modLine(line_num, text):
+  with open(name, 'r') as code:
+    lines = code.readlines()
+        
+  if 0 <= line_num < len(lines):
+    lines[line_num] = new_text + '\n'
+        
+    with open('code', 'w') as code:
+      code.writelines(lines)
+  else: print("Invalid line space")
