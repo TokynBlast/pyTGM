10f9adde7591021f020d23522f9e3de6d653450f
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-11 19:30:21 -0800

    Create sound.py and add name set variable

---
+++ pytgm/sound.py
@@ -0,0 +1,60 @@
+def file(path):
+    try:
+        from winsound import PlaySound as PS, SND_FILENAME
+        PS(path, SND_FILENAME)
+    
+    except:
+        #macOS
+
+        if os.uname().sysname == 'Darwin':
+            system(f'afplay {path}')
+         
+        try: uname()
+        except: from os import uname
+        if uname().sysname == 'Darwin':
+            try: system('echo  ')
+            except: import system
+            os.system(f'afplay {path}')
+            
+        #Linux
+        else:
+            try: system('echo  ')
+            except: from os import system
+            system(f'aplay {path}')
+
+class frequency
+    @staticmethod
+    def big(frequency, duration, name, sample_rate=44100, volume=0.5):
+        import wave, math, os, array
+        n_samples = int(sample_rate * duration)
+    
+        samples = array.array('h')
+        
+        for i in range(n_samples):
+            t = i / sample_rate
+            sample_value = volume * math.sin(2 * math.pi * frequency * t)
+            samples.append(int(sample_value * 32767))
+    
+        with wave.open(name, 'w') as wf:
+            wf.setnchannels(1)
+            wf.setsampwidth(2)
+            wf.setframerate(sample_rate)
+            wf.writeframes(samples.tobytes())
+    @staticmethod
+    def small(frequency, duration, name, sample_rate=44100, volume=0.5):
+        import wave, math
+    
+        n_samples = int(sample_rate * duration)
+        samples = []
+        for i in range(n_samples):
+            sample = volume * math.sin(2 * math.pi * frequency * (i / sample_rate))
+            samples.append(int(sample * 32767))  # Convert to 16-bit PCM format
+    
+        # Write to a temporary WAV file
+        with wave.open(name, 'w') as wf:
+            wf.setnchannels(1)
+            wf.setsampwidth(2)
+            wf.setframerate(sample_rate)
+            wf.writeframes(bytearray(samples))
+    
+    
