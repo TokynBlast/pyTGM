b91f6543f144c164ffd907d2e94cdf576e5fc27d
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-13 13:19:08 -0800

    Create LocalServer.py and remove time import

---
+++ pytgm/LocalServer.py
@@ -0,0 +1,64 @@
+import socket
+import threading
+
+def server(host='localhost', PORT_='5000'):
+    def server_(host,PORT_):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((HOST, PORT_))
+        server_socket.listen()
+        
+        clients = []
+        
+        def handle_client(client):
+            while True:
+                try:
+                    message = client.recv(1024)
+                    for client in clients:
+                        client.send(message)
+                except:
+                    clients.remove(client)
+                    client.close()
+                    break
+    
+        def receive():
+            while True:
+                client, address = server_socket.accept()
+                print(f"Connected with {address}")
+                clients.append(client)
+                client.send('Connected to the server!'.encode('ascii'))
+                thread = threading.Thread(target=handle_client, args=(client,))
+                thread.start()
+        
+        print("Server is waiting for connections...")
+        receive()
+    
+    server_thread = threading.Thread(target=server_)
+    server_thread.start()
+
+def client(host, PORT_):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, PORT_))
+    
+    def receive():
+        messages = []
+        while True:
+            try:
+                message = client_socket.recv(1024).decode('ascii')
+                messages.append(message)
+            except:
+                print("An error occurred!\nDisconnecting from the server.")
+                client_socket.close()
+                break
+        return messages
+    
+    def write():
+        while True:
+            message = To_send
+            if message:
+                client_socket.send(message.encode('ascii'))
+
+    receive_thread = threading.Thread(target=receive)
+    receive_thread.start()
+    
+    write_thread = threading.Thread(target=write)
+    write_thread.start()
