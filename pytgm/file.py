71a7a37c8704a80de7fd7e3640a9318d36f7ad4b
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-14 10:30:35 -0800

    Update file.py

--- pytgm/file.py
+++ pytgm/file.py
@@ -1,16 +1,23 @@
-def readLine(name, line=0):
-  x = open(name, 'r')
-  x.readlines()[line]
-  return x
-        
-    
-def modLine(text, line=0):
-  with open(name, 'r') as code:
+def read_line(name, line=0):
+    """
+    Returns a single line of a file
+    """
+    x = open(name, 'r')
+    x.readlines()[line]
+    return x
+
+def mod_line(text, line=0):
+    """
+    Modifies a line of a file
+    This will be able to do more than just a single line in the future
+    Likely, multiple
+    """
+    with open(name, 'r') as code:
     lines = code.readlines()
-        
-  if 0 <= line_num < len(lines):
-    lines[line_num] = new_text + '\n'
-        
+
+    if 0 <= line_num < len(lines):
+        lines[line_num] = new_text + '\n'
+
     with open('code', 'w') as code:
-      code.writelines(lines)
-  else: print("Invalid line space")
+        code.writelines(lines)
+    else: print("Invalid line space"
