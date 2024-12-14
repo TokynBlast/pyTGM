06a13e769feb47715e521c1d7d0376adb28a4f46
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-13 18:12:09 -0800

    Made board.py follow PEP 8

--- pytgm/board.py
+++ pytgm/board.py
@@ -1,6 +1,9 @@
 boards = []
 
 def new(title, component=None, value=None):
+    """
+    Makes a board, with a component and a value
+    """
     for board in boards:
         if title in board:
             if component:
@@ -9,19 +12,27 @@ def new(title, component=None, value=None):
     boards.append({title: {component: value}} if component else {title: {}})
 
 def remove(title, component=None):
-    for board in boards:
+    """
+    Removes a board or component of a board
+    """
+    for board in boards[:]:  # Create a shallow copy of the list for iteration
         if title in board:
             if component:
                 if component in board[title]:
                     del board[title][component]
                     if not board[title]:
                         boards.remove(board)
-                return
             else:
                 boards.remove(board)
-                return
 
 def modify(title, component, function):
-    for board in boards:
+    """
+    Modify a board's components
+
+    In the future, this will become a class, where you can:
+    
+    """
+    for board in boards[:]:  # Iterate over a shallow copy of the list
         if title in board and component in board[title]:
             board[title][component] = eval(f"{board[title][component]} {function}")
+
