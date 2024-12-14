b6c1e63c5d101d78b663306f592a2151d48c7352
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-13 17:27:42 -0800

    Updated to follow PEP guidelines

--- pytgm/__init__.py
+++ pytgm/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['random','num','integer','binary','choose','seq','modify','shuffle','duplicate','remove','file','readLine','modLine','graphics','cls','color','res','bold','italic','underline','terd','geky','click','sound','file','frequency','big','small','board','boards','add','remove','modify','LocalServer','server','client','b64','table_','tableGen','tableSet','encode','decode']
+__all__ = ['random','num','integer','binary','choose','seq','modify','shuffle','duplicate','remove','file','read_line','mod_ine','graphics','cls','color','res','bold','italic','underline','terd','geky','click','sound','file','frequency','big','small','board','boards','add','remove','modify','local_erver','server','client','b64','table_','table_gen','table_set','encode','decode']
 __url__ = 'https://github.com/TokynBlast/pyTGM'
 __homepage__ = 'https://pytgm.tokynblast.space/home'
 __download_url__ = 'https://pypi.org/tokynblast'
@@ -7,10 +7,10 @@ __bug_tracker_url__ = 'https://github.com/TokynBlast/pyTGM/issues'
 __source_code_url__ = 'https://github.com/TokynBlast/pyTGM/tree/main'
 __changelog_url__ = 'https://github.com/TokynBlast/pyTGM/blob/main/CHANGELOG.txt'
 
-import .random as random
-import .b64 as b64
-import .file as file
-import .board as board
-import .sound as sound
-import .graphics as graphics
-import .LocalServer as LocalServer
+import . random as random
+import . b64 as b64
+import . file as file
+import . board as board
+import . sound as sound
+import . graphics as graphics
+import . local_server as local_server
