bb40e9df116781db6e880644eb5c2b689af70164
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-14 11:15:04 -0800

    Changed folders to a file

--- pytgm/random/num/integer.py
+++
@@ -1,7 +0,0 @@
-def integer(min_value, max_value, seed=None):
-    if seed == None:
-      try: seed = int(time() * 1000)
-      except: from time import time; seed = int(time() * 1000)
-              
-      number = (seed % (max_value - min_value + 1)) + min_value
-      return number
