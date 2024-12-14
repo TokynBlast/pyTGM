b8d41eb818266090d665e302b5a3f5f2158f4a5f
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-14 13:06:46 -0800

    Fixed geky for pylint and removed board.py

--- pytgm/terd/geky.py
+++ pytgm/terd/geky.py
@@ -1,3 +1,6 @@
+"""
+Used for detecting key presses
+"""
 def geky(times=1):
     try:
         from msvcrt import getch as g
@@ -5,11 +8,16 @@ def geky(times=1):
             k = g()
             if k == b'\xe0':
                 k = g()
-                if k == b'H': return 'ArrowUp'
-                elif k == b'P': return 'ArrowDown'
-                elif k == b'K': return 'ArrowLeft'
-                elif k == b'M': return 'ArrowRight'
-            else: return key.decode()
+                if k == b'H':
+                    return 'ArrowUp'
+                elif k == b'P':
+                    return 'ArrowDown'
+                elif k == b'K':
+                    return 'ArrowLeft'
+                elif k == b'M':
+                    return 'ArrowRight'
+            else:
+                return key.decode()
 
     except:
         from sys import stdin
@@ -23,11 +31,16 @@ def geky(times=1):
                 c = stdin.read(1)
                 if c == '\x1b':
                     c = stdin.read(2)
-                    if c == '[A': return 'ArrowUp'
-                    elif c == '[B': return 'ArrowDown'
-                    elif c == '[C': return 'ArrowRight'
-                    elif c == '[D': return 'ArrowLeft'
-                    else: return c
+                    if c == '[A':
+                        return 'ArrowUp'
+                    elif c == '[B':
+                        return 'ArrowDown'
+                    elif c == '[C':
+                        return 'ArrowRight'
+                    elif c == '[D':
+                        return 'ArrowLeft'
+                    else:
+                        return c
                 else:
                     if c == ' ': return 'space'
                     else: return c
