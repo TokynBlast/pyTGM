54f38374a4d1149f93a3df138018aeba28d04861
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-14 11:13:48 -0800

    Fixed importing

--- pytgm/__init__.py
+++ pytgm/__init__.py
@@ -1,4 +1,13 @@
-__all__ = ['random','num','integer','binary','choose','seq','modify','shuffle','duplicate','remove','file','read_line','mod_ine','graphics','cls','color','res','bold','italic','underline','terd','geky','click','sound','file','frequency','big','small','board','boards','add','remove','modify','local_erver','server','client','b64','table_','table_gen','table_set','encode','decode']
+"""
+Imports all modules for the library
+"""
+__all__ = ['random','num','integer','binary','choose','seq','modify','shuffle',
+           'duplicate','remove','file','read_line','mod_ine','graphics','cls',
+           'color','res','bold','italic','underline','terd','geky','click','sound',
+           'file','frequency','big','small','board','boards','add','remove',
+           'modify','local_server','server','client','b64','table_','table_gen',
+           'table_set','encode','decode']
+
 __url__ = 'https://github.com/TokynBlast/pyTGM'
 __homepage__ = 'https://pytgm.tokynblast.space/home'
 __download_url__ = 'https://pypi.org/tokynblast'
@@ -11,9 +20,9 @@ from . import random, b64, file, board, sound, graphics, local_server, terd
 
 # Random imports
 from .random import num, seq
-from .random.num import integer, binary
-from .random.seq import modify
-from .random.seq.modify import shuffle, duplicate, remove
+from num import integer, binary
+from seq import modify, choose
+from modify import shuffle, duplicate, remove
 
 # b64 Imports
 from .b64 import Table, encode, decode
@@ -26,7 +35,8 @@ from .file import read_line, mod_line
 from .board import boards, new, remove, modify
 
 # Sound imports
-from .sound import file, big, small
+from .sound import file, frequency
+from .sound.frequency import big, small
 
 # Graphic imports
 from .graphics import cls, color, markup, res
