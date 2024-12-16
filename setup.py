d8b6cc5ca09e24fe965d7b57c325dc75c186f41f
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-16 06:05:12 -0800

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -30,11 +30,9 @@ setup(
     name='pyTGM',
     version='4.0.0',
     description='Terminal Game Maker',
-    long_description = (  # pylint: disable=syntax-error
-    open('README.txt', encoding='utf-8').read() +
-    '\n\n' +
-    open('CHANGELOG.txt', encoding='utf-8').read()
-    )
+    long_description=open('README.txt', encoding='utf-8').read() +
+        '\n\n' +
+        open('CHANGELOG.txt', encoding='utf-8').read(),
     url='https://youtube.tokynblast.space/programming/libraries/pytgm/init',
     author=MAINTAIN_AUTH,
     author_email=MAINTAIN_AUTH_CONTACT,
