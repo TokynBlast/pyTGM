e674e1cb6020ff21d20bbacb4f9ddcce74f88e2c
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-15 19:25:16 -0800

    Fixed doubling of __init__

--- pytgm/__init__.py
+++ pytgm/__init__.py
@@ -41,57 +41,7 @@ from pytgm.sound import play, generate
 
 # Graphics Imports
 from pytgm.graphics import cls, color, Markup, RESET
-from pytgm.graphics.Markup import bold, italic, """
-This is the init file for the pyTGM library.
-
-It imports and exposes various modules and functionalities for:
-- File handling
-- Graphics rendering
-- Sound generation
-- Base64 encoding/decoding
-- Local server operations
-- Miscellaneous tools
-"""
-
-__all__ = [
-    'file', 'read_line', 'mod_line',
-    'graphics', 'cls', 'color', 'RESET', 'Markup', 'bold', 'italic', 'underline',
-    'terd', 'geky', 'click',
-    'sound', 'generate',
-    'local_server', 'server', 'client',
-    'b64', 'table', 'gen', 'encode', 'decode'
-]
-
-__url__ = 'https://github.com/TokynBlast/pyTGM'
-__homepage__ = 'https://pytgm.tokynblast.space/home'
-__download_url__ = 'https://pypi.org/tokynblast'
-__docs_url__ = 'https://pytgm.tokynblast.space/documentation/use'
-__bug_tracker_url__ = 'https://github.com/TokynBlast/pyTGM/issues'
-__source_code_url__ = 'https://github.com/TokynBlast/pyTGM/tree/main'
-__changelog_url__ = 'https://github.com/TokynBlast/pyTGM/blob/main/CHANGELOG.txt'
-
-from pytgm import b64, file, sound, graphics, local_server, terd
-
-# b64 Imports
-from pytgm.b64 import Table, encode, decode
-from pytgm.b64 import table_ as table, gen
-
-# File Imports
-from pytgm.file import read_line, mod_line
-
-# Sound Imports
-from pytgm.sound import play, generate
-
-# Graphics Imports
-from pytgm.graphics import cls, color, Markup, RESET
 from pytgm.graphics.Markup import bold, italic, underline
-
-# Online Imports
-from pytgm.local_server import server, client
-
-# terd Imports
-from pytgm.terd import geky, click
-underline
 
 # Online Imports
 from pytgm.local_server import server, client
