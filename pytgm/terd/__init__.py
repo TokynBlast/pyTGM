5a29545a559b8b6b39383a2883a1456af2e631a5
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-15 18:13:36 -0800

    Import errors for click and geky fixed

--- pytgm/terd/__init__.py
+++ pytgm/terd/__init__.py
@@ -2,6 +2,7 @@
 Used for detecting non-standard inputs into the terminal.
 """
 
-import click, geky
+from pytgm.terd import click # pylint: disabl=import-error
+from pytgm.terd import geky
 
 __all__ = ['click','geky']
