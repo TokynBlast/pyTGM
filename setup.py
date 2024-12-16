215873b6e83735b65f9308365f2c3f3ba55c81e3
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-16 07:31:36 -0800

    Made pylint ignore not opening a file using with statement

--- setup.py
+++ setup.py
@@ -30,9 +30,9 @@ setup(
     name='pyTGM',
     version='4.0.0',
     description='Terminal Game Maker',
-    long_description=open('README.txt', encoding='utf-8').read() +
+    long_description=open('README.txt', encoding='utf-8').read() + # pylint: disable=consider-using-with
         '\n\n' +
-        open('CHANGELOG.txt', encoding='utf-8').read(),
+        open('CHANGELOG.txt', encoding='utf-8').read(), # pylint: disable=consider-using-with
     url='https://youtube.tokynblast.space/programming/libraries/pytgm/init',
     author=MAIT_AUTH,
     author_email=MAIT_AUTH_CONT,
