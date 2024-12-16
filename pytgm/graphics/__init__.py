ccef832407c933f02ca236d6b1c5c44876cb9200
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-16 06:00:29 -0800

    Moved bold, italic, and underline out of Markuo and into an __init__ file

--- pytgm/graphics/__init__.py
+++ pytgm/graphics/__init__.py
@@ -1,11 +1,14 @@
-# pytgm/graphics/__init__.py
-
+"""
+Exposes cls and color, while adding RESET, bold, italic and underline
+"""
 from .color import color
 from .cls import cls
-from .Markup import Markup, bold, italic, underline  # Import other symbols as needed
 
 RESET = "\x1b[0m"
 
-__all__ = ["cls", "Markup", "bold", "italic", "underline", "RESET"]
+bold = '\x1b[1m'
+italic = '\x1b[3m'
+underline = '\x1b[4m'
 
 
+__all__ = ["cls", "bold", "italic", "underline", "RESET"]
