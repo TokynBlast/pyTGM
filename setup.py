fcbc8f77a35c6e783667e684f171f1a235e61415
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-16 15:06:50 -0800

    Added pytgm to the dir of sources for click.c

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     install_requires=[''],
     ext_modules=[Extension(
         'click',
-        sources=['terd/click.c'],
+        sources=['pytgm/terd/click.c'],
         extra_compile_args=extra_compile_args[platform],
     )],
     python_requires=">=3.13",
