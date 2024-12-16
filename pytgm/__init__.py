f489bae206268dd02a3741a6043712b444bb7a9c
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-15 19:22:51 -0800

    Removed terd/__init.py, and fixed __init__.py code

--- pytgm/__init__.py
+++ pytgm/__init__.py
@@ -1,4 +1,15 @@
-# pylint: disable=module-doc-string
+"""
+This is the init file for the pyTGM library.
+
+It imports and exposes various modules and functionalities for:
+- File handling
+- Graphics rendering
+- Sound generation
+- Base64 encoding/decoding
+- Local server operations
+- Miscellaneous tools
+"""
+
 __all__ = [
     'file', 'read_line', 'mod_line',
     'graphics', 'cls', 'color', 'RESET', 'Markup', 'bold', 'italic', 'underline',
@@ -20,7 +31,7 @@ from pytgm import b64, file, sound, graphics, local_server, terd
 
 # b64 Imports
 from pytgm.b64 import Table, encode, decode
-from pytgm.b64.Table import gen, table_ as table
+from pytgm.b64 import table_ as table, gen
 
 # File Imports
 from pytgm.file import read_line, mod_line
@@ -29,9 +40,58 @@ from pytgm.file import read_line, mod_line
 from pytgm.sound import play, generate
 
 # Graphics Imports
-from pytgm.graphics import cls, color, Markup
-from pytgm.graphics import RESET
+from pytgm.graphics import cls, color, Markup, RESET
+from pytgm.graphics.Markup import bold, italic, """
+This is the init file for the pyTGM library.
+
+It imports and exposes various modules and functionalities for:
+- File handling
+- Graphics rendering
+- Sound generation
+- Base64 encoding/decoding
+- Local server operations
+- Miscellaneous tools
+"""
+
+__all__ = [
+    'file', 'read_line', 'mod_line',
+    'graphics', 'cls', 'color', 'RESET', 'Markup', 'bold', 'italic', 'underline',
+    'terd', 'geky', 'click',
+    'sound', 'generate',
+    'local_server', 'server', 'client',
+    'b64', 'table', 'gen', 'encode', 'decode'
+]
+
+__url__ = 'https://github.com/TokynBlast/pyTGM'
+__homepage__ = 'https://pytgm.tokynblast.space/home'
+__download_url__ = 'https://pypi.org/tokynblast'
+__docs_url__ = 'https://pytgm.tokynblast.space/documentation/use'
+__bug_tracker_url__ = 'https://github.com/TokynBlast/pyTGM/issues'
+__source_code_url__ = 'https://github.com/TokynBlast/pyTGM/tree/main'
+__changelog_url__ = 'https://github.com/TokynBlast/pyTGM/blob/main/CHANGELOG.txt'
+
+from pytgm import b64, file, sound, graphics, local_server, terd
+
+# b64 Imports
+from pytgm.b64 import Table, encode, decode
+from pytgm.b64 import table_ as table, gen
+
+# File Imports
+from pytgm.file import read_line, mod_line
+
+# Sound Imports
+from pytgm.sound import play, generate
+
+# Graphics Imports
+from pytgm.graphics import cls, color, Markup, RESET
 from pytgm.graphics.Markup import bold, italic, underline
+
+# Online Imports
+from pytgm.local_server import server, client
+
+# terd Imports
+from pytgm.terd import geky, click
+underline
 
 # Online Imports
 from pytgm.local_server import server, client
--- pytgm/terd/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Used for detecting non-standard inputs into the terminal.
-"""
-
-from pytgm.terd import click # pylint: disabl=import-error
-from pytgm.terd import geky
-
-__all__ = ['click','geky']
