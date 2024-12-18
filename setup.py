bcc957e874756fcc30fb696ef706bde9014fafe4
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-18 13:09:57 -0800

    Made setup.py more dynamic

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ Handles package configuration and extension building.
 """
 
 from platform import system as sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 classifiers = [
@@ -18,57 +18,51 @@ classifiers = [
     'Programming Language :: Python :: 3.13',
 ]
 
-if sys().startswith("Win"):
-    extra_compile_args = ["/std:c++latest", "/EHsc", "/bigobj"]
-    extra_link_args = ["User32.lib"]
-    sources = ["pytgm/terd/click.cpp"]
-elif sys().startswith("linux"):
-    extra_compile_args = ["-std=c++11", "-O3", "-Wall", "-fPIC"]
-    extra_link_args = []
-    sources = ["pytgm/terd/click.cpp"]
-elif sys().startswith("darwin"):
-    extra_compile_args = ["-std=c++11", "-O3", "-Wall", "-fPIC"]
-    extra_link_args = []
-    sources = ["pytgm/terd/click.cpp"]
-else:
-    raise RuntimeError(f"Unsupported platform: {sys()}")
+class BuildExt(build_ext):
+    def build_extensions(self):
+        os_type = sys()
+        if os_type == "Windows":
+            for ext in self.extensions:
+                ext.extra_compile_args = ["/std:c++latest", "/EHsc", "/bigobj"]
+                ext.extra_link_args = ["User32.lib"]
+        elif os_type == "Linux":
+            for ext in self.extensions:
+                ext.extra_compile_args = ["-std=c++11", "-O3", "-Wall", "-fPIC"]
+                ext.extra_link_args = []
+        elif os_type == "Darwin":
+            for ext in self.extensions:
+                ext.extra_compile_args = ["-std=c++11", "-O3", "-Wall", "-fPIC"]
+                ext.extra_link_args = []
+        else:
+            raise RuntimeError(f"Unsupported platform: {os_type}")
+        super().build_extensions()
 
 MAIT_AUTH = 'Tokyn Blast'
 MAIT_AUTH_CONT = 'tokynblast@gmail.com'
 
 click_extension = Pybind11Extension(
-    name="click",
-    sources=sources,
-    extra_compile_args=extra_compile_args,
-    extra_link_args=extra_link_args,
+    name="pytgm.terd.click",
+    sources=["pytgm/terd/click.cpp"],
+    language="c++",
 )
 
 setup(
     name='pyTGM',
     version='4.0.6',
     description='Terminal Game Maker',
-    long_description=(open('README.md', encoding='utf-8').read() + # pylint: disable=consider-using-with
-                      '\n\n' +
-                      open('CHANGELOG.txt', encoding='utf-8').read()), # pylint: disable=consider-using-with
+    long_description=(open('README.md', encoding='utf-8').read() + '\n\n' +
+                      open('CHANGELOG.txt', encoding='utf-8').read()),
     long_description_content_type='text/markdown',
     url='https://github.com/TokynBlast/pyTGM',
     author='Tokyn Blast',
     author_email='tokynblast@gmail.com',
     license='Bspace',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.13',
-    ],
+    classifiers=classifiers,
     keywords='game, game maker, terminal, tools, pytgm, terminal input',
     packages=find_packages(),
     install_requires=[],
     ext_modules=[click_extension],
-    cmdclass={"build_ext": build_ext},
+    cmdclass={"build_ext": BuildExt},
     python_requires=">=3.13",
     platforms=["Windows", "Linux", "MacOS"],
 )
