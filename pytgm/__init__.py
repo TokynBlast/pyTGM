68a7e7b88a6fd41e32f7a77f504d0c88af3e41bb
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-18 11:34:21 -0800

    Added importing graphics

--- pytgm/__init__.py
+++ pytgm/__init__.py
@@ -9,8 +9,12 @@ It imports and exposes various modules and functionalities for:
 - Local server operations
 """
 
-print(f'{graphics.color(255,255,0)}WARNING: file.read_line() will soon be {graphics.color(255,0,0)}depreciated!{graphics.RESET}')
-print(f'{graphics.color(255,255,0)}WARNING: sound.generate will soon be {graphics.color(255,0,0)}depreciated!{graphics.RESET}\nAnd sound.play() will be renamed as sound()')
+import .graphics
+
+print(f'{graphics.color(255,255,0)}WARNING: file.read_line() will soon be \
+{graphics.color(255,0,0)}depreciated!{graphics.RESET}')
+print(f'{graphics.color(255,255,0)}WARNING: sound.generate will soon be \
+{graphics.color(255,0,0)}depreciated!{graphics.RESET}\nAnd sound.play() will be renamed as sound()')
 
 __all__ = [
     'Table', 'encode', 'decode',
