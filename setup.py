139bb044205e5eb5c62da22326d96d8feb8e9586
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-18 08:55:31 -0800

    Updated ver #, and changed how the OS is detected

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ Setup script for the pyTGM package.
 Handles package configuration and extension building.
 """
 
-from sys import platform
-from setuptools import setup, find_packages, Extension
+import platform
+from setuptools import setup, find_packages
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 classifiers = [
@@ -18,11 +18,13 @@ classifiers = [
     'Programming Language :: Python :: 3.13',
 ]
 
+current_os = platform.system().lower()
+
 extra_compile_args = {
     'linux': ['-std=c++11', '-O3', '-Wall', '-fPIC'],
     'darwin': ['-std=c++11', '-O3', '-Wall', '-fPIC'],
-    'win32': ['/std:c++11', '/O2', '/W4']
-}
+    'windows': ['/std:c++11', '/O2', '/W4']
+}.get(current_os, [])
 
 extensions = [
     Pybind11Extension(
@@ -36,7 +38,7 @@ MAIT_AUTH_CONT = 'tokynblast@gmail.com'
 
 setup(
     name='pyTGM',
-    version='4.0.5',
+    version='4.0.6',
     description='Terminal Game Maker',
     long_description = (open('README.md', encoding='utf-8').read() + '\n\n' + open('CHANGELOG.txt', encoding='utf-8').read()), # pylint: disable=consider-using-with, line-too-long
     long_description_content_type='text/markdown',
