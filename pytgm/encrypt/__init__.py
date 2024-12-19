6bdf89a0291c0053dc37c1a40424e92ff7a24793
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-19 11:37:25 -0800

    Fixed imports and variables

--- pytgm/encrypt/__init__.py
+++ pytgm/encrypt/__init__.py
@@ -3,6 +3,6 @@ Exposes ecryptions
 """
 
 from .b64 import Table, encode, decode
-from .sha256 import encode
+from .sha256 import encode as sha256
 
 __all__ = ['Table', 'encode', 'decode', 'encode']
