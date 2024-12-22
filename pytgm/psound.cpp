ec8cb6f19f2e89b56e8793fb949358b5009eec7e
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2024-12-21 20:42:43 -0800

    Added play and generate

--- pytgm/psound.cpp
+++ pytgm/psound.cpp
@@ -51,6 +51,42 @@ std::optional<std::string> psound(const std::string& file) {
     }
 }
 
+void play(const file) {
+    if (fs::exist(file)) {
+        psound(file)
+    }
+
+    const std::string yellow = '\x1b[38;2;255;255;0m';
+    const std::string red = '\x1b[38;2;255;0;0m';
+    const std::string res = '\x1b[0m';
+
+    std::cout << yellow << "WARNING: " << red << "sound.play() will become psound(str) in v4.2.1\n"
+              << "After that, sound.play(str) will no longer be valid.\n"
+              << "You should being using psound(str) now." << reset << endl;
+    }
+
+void generate(std::string p1 = "", std::string p2 = "", std::string p3 = "", 
+              std::string p4 = "", std::string p5 = "") {
+    (void)p1;
+    (void)p2;
+    (void)p3;
+    (void)p4;
+    (void)p5;
+
+
+    const std::string yellow = "\033[38;2;255;255;0m";
+    const std::string red = "\033[38;2;255;0;0m";
+    const std::string reset = "\033[0m";
+
+    std::cout << yellow << "WARNING: " << red 
+              << "sound.generate() is no longer implemented\n"
+              << "This change was made in 4.1.0, this function will be removed entirely in 4.2.0\n"
+              << "If you want to play a sound, use psound(std::string file_path)" 
+              << reset << std::endl;
+}
+
+
+
 PYBIND11_MODULE(pytgm, m) {
     m.doc() = "C++ implementation of playing a sound from the terminal.";
     m.def("psound", &psound, "Plays a sound from the terminal.");
