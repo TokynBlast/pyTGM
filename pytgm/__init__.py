9b156923783acf0052f39d2093f83a7f4633a541
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-01-06 19:36:05 -0800

    Fixed missing comma

--- pytgm/__init__.py
+++ pytgm/__init__.py
@@ -9,7 +9,7 @@ It imports and exposes various modules and functionalities for:
 """
 
 __all__ = [
-    'sha256', 'b64'
+    'sha256', 'b64',
     'rep','fm_line',
     'psound',
     'server', 'client',
