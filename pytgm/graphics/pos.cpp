8ddc46ede75bde8bda727724919aa0be8f020f53
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-01-08 10:00:35 -0800

    Added pybind11

--- pytgm/graphics/pos.cpp
+++ pytgm/graphics/pos.cpp
@@ -6,3 +6,8 @@ std::string pos(const int x, const int y) -> std::string {
     ss << "\003[" << x << ";" << y << "H";
     return ss.str();
 }
+
+PYBIND11_MODULE(pos, m) {
+    m.doc() = "Places the mouse at a specified point";
+    m.def("pos", &pos, "Places the mouse at a specified point");
+}
