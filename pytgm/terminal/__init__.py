50ed101bd2b341ca55469b9c13474b48f9bf19c5
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-01-08 10:10:51 -0800

    Updated __init__

--- pytgm/terminal/__init__.py
+++ pytgm/terminal/__init__.py
@@ -4,4 +4,15 @@ Imports all files needed for terd
 from .click import click # pylint: disable=import-error
 from .geky import geky
 
-__all__ = ['click', 'geky']
+from .color import color #pylint:disable=import-error
+from .cls import cls #pylint:disable=import-error
+from .pos import pos #pylint:disable=import-error
+
+RESET = "\x1b[0m"
+
+BOLD = '\x1b[1m'
+ITALIC = '\x1b[3m'
+UNDERLINE = '\x1b[4m'
+
+__all__ = ['click', 'geky',
+           'cls', 'color', 'pos', 'RESET', 'ITALIC', 'BOLD', 'UNDERLINE']
