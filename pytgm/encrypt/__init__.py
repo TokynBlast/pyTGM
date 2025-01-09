21089df77028ff45089bf3b921a0aa17b808e712
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-01-09 15:44:41 -0800

    Added import error ignore for hk32

--- pytgm/encrypt/__init__.py
+++ pytgm/encrypt/__init__.py
@@ -3,7 +3,7 @@ Exposes ecryptions
 """
 
 from .b64 import Table, encode, decode
-from .hk32 import __all__ as hk32_all
+from .hk32 import encode, decode #pylint:disable=import-error
 from .sha256 import encode as sha256
 
-__all__ = ['Table', 'sha256', hk32_all, 'encode', 'decode']
+__all__ = ['Table', 'sha256', 'b64.encode', 'b64.decode', 'hk32.encode', 'hk32.decode']
