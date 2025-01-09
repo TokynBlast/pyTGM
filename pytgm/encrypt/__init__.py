d795a28d4859c45a95a4124ac661d11cbdb5595e
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-01-09 09:59:00 -0800

    Updated imports and __all__

--- pytgm/encrypt/__init__.py
+++ pytgm/encrypt/__init__.py
@@ -4,5 +4,6 @@ Exposes ecryptions
 
 from .b64 import Table, encode, decode
 from .sha256 import encode as sha256
+from .hk36 import encode, decode #pylint:disable=import-error
 
-__all__ = ['Table', 'encode', 'decode', 'encode']
+__all__ = ['Table', 'encode', 'decode', 'encode', 'decode', 'sha256']
