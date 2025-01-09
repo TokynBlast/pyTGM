0bba8e3ee4344554cec64b98e56f2b285cb019fb
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-01-09 15:41:15 -0800

    Removed b64_all

--- pytgm/encrypt/__init__.py
+++ pytgm/encrypt/__init__.py
@@ -2,8 +2,8 @@
 Exposes ecryptions
 """
 
-from .b64 import Table, __all__ as b64_all
-from .hk32 import __all__ as hk32_all #pylint:disable=import-error
+from .b64 import Table, encode, decode
+from .hk32 import __all__ as hk32_all
 from .sha256 import encode as sha256
 
-__all__ = ['Table', 'sha256', b64_all, hk32_all]
+__all__ = ['Table', 'sha256', hk32_all, 'encode', 'decode']
