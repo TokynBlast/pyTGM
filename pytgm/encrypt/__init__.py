7a609378dda49b4fb051f3253474291ae4813294
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-01-09 16:31:16 -0800

    Made an hk32 class

--- pytgm/encrypt/__init__.py
+++ pytgm/encrypt/__init__.py
@@ -1,9 +1,16 @@
 """
 Exposes ecryptions
 """
-
+import .hk32 #pylint:disable=import-error
 from .b64 import Table, encode, decode
-import hk32 #pylint:disable=import-error
 from .sha256 import encode as sha256
+
+class hk32:
+    @staticmethod
+    def encode(data, key):
+        hk32.encode()
+    @staticmethod
+    def decode(data, key):
+        hk32.decode()
 
 __all__ = ['Table', 'sha256', 'encode', 'decode', 'hk32.encode', 'hk32.decode']
