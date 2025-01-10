1782bd84c2e01b3c05cf8f5e5a6b8cff1d8a5af1
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-01-09 16:40:33 -0800

    Moved reimported ignore

--- pytgm/encrypt/__init__.py
+++ pytgm/encrypt/__init__.py
@@ -1,8 +1,9 @@
 """
 Exposes ecryptions
 """
-from .hk32 import encode, decode #pylint:disable=import-error
+from .hk32 import encode, decode #pylint:disable=import-error,reimported
+
 from .b64 import Table, encode, decode
 from .sha256 import encode as sha256
 
-__all__ = ['Table', 'sha256', 'encode', 'decode', 'encode', 'decode'] #pylint:disable=reimported
+__all__ = ['Table', 'sha256', 'encode', 'decode', 'encode', 'decode']
