03de293bc13dfa74a0756fab74ad3937dd3c4998
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-01-09 16:25:43 -0800

    Changed how hk32 is imported

--- pytgm/encrypt/__init__.py
+++ pytgm/encrypt/__init__.py
@@ -3,7 +3,7 @@ Exposes ecryptions
 """
 
 from .b64 import Table, encode, decode
-from .hk32 import encode, decode #pylint:disable=import-error
+from .hk32 import * #pylint:disable=import-error
 from .sha256 import encode as sha256
 
-__all__ = ['Table', 'sha256', 'b64.encode', 'b64.decode', 'hk32.encode', 'hk32.decode']
+__all__ = ['Table', 'sha256', 'encode', 'decode', 'hk32.encode', 'hk32.decode']
