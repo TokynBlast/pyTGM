a545c35a4bc5856100ee6f5214bca7a82548e586
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-13 12:33:07 -0800

    Added doc-strings

--- pytgm/pattern/rect.py
+++ pytgm/pattern/rect.py
@@ -1,10 +1,12 @@
+""" Clears the terminal as it moves along """
 import time as t
 import sys
 
 def rect(width, height, time=3, char=" "):
-    total_steps = (height * (height + 1)) // 2 + (height * (height - 1)) // 2 + (max(width - height, 0) * height)
+    """ Moves acress the terminal, and prints a space, to clear the screen """
+    total_steps = (height * (height + 1)) // 2 + (height * (height - 1)) // 2 + (max(width - height, 0) * height) #pylint-ignore: line-too-long
     step_time = time / total_steps  # Calculate time per step
-    
+
     # Left side
     for i in range(height):
         line_length = min(i + 1, width)
@@ -12,14 +14,14 @@ def rect(width, height, time=3, char=" "):
             sys.stdout.write(f"\x1b[{j + 1};{i - j + 1}H{char}")
             sys.stdout.flush()
             t.sleep(step_time)
-    
+
     # middle
     for i in range(height, width):
         for j in range(height):
             sys.stdout.write(f"\x1b[{j + 1};{i - j + 1}H{char}")
             sys.stdout.flush()
             t.sleep(step_time)
-    
+
     # right side
     for i in range(height):
         for j in range(i, height):
