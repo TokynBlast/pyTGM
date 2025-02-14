00fbe6c885400ce1978dde6cd2e92fa0e7ffb0a3
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-14 10:39:15 -0800

    Fixed m.doc

--- pytgm/encrypt/hk512.cpp
+++ pytgm/encrypt/hk512.cpp
@@ -141,7 +141,7 @@ std::string decode(const std::string& data, const std::string& key) {
 }
 
 PYBIND11_MODULE(hk512, m) {
-    m.doc("Used to encrypt things into an hk512 number");
+    m.doc() = "Used to encrypt things into an hk512 number";
     m.def("encode", &encode, "Encode data hk512",
         py::arg("data"), py::arg("key"));
     m.def("decode", &decode, "Decode an hk512 string",
