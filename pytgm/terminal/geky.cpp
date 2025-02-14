984afabf364359dc70687ad6c170ab2231d6471c
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-14 03:40:49 +0000

    Fixed for going between windows and Unix-like OS

--- pytgm/terminal/geky.cpp
+++ pytgm/terminal/geky.cpp
@@ -6,63 +6,53 @@
 #include <Python.h>
 #include <pybind11/pybind11.h>
 
+#ifdef _WIN32
+#include <conio.h> // For _getch() on Windows
+#else
+#include <fcntl.h> // For non-blocking input
+#endif
+
 std::string geky(int times = 1) {
+    struct termios old, newt;
+    tcgetattr(STDIN_FILENO, &old);
+    newt = old;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    std::string result;
     try {
         for (int i = 0; i < times; ++i) {
-            int k = _getch();
-            if (k == 0 || k == 224) {
-                k = _getch();
-                if (k == 72) {
-                    return "ArrowUp";
-                } else if (k == 80) {
-                    return "ArrowDown";
-                } else if (k == 75) {
-                    return "ArrowLeft";
-                } else if (k == 77) {
-                    return "ArrowRight";
+            char c;
+#ifdef _WIN32
+            c = _getch(); // Use _getch() on Windows
+#else
+            read(STDIN_FILENO, &c, 1); // Use read() on Unix-like systems
+#endif
+            if (c == 27) { // Escape character
+                char seq[2];
+                read(STDIN_FILENO, seq, 2);
+                if (seq[0] == '[') {
+                    if (seq[1] == 'A') {
+                        return "ArrowUp";
+                    } else if (seq[1] == 'B') {
+                        return "ArrowDown";
+                    } else if (seq[1] == 'C') {
+                        return "ArrowRight";
+                    } else if (seq[1] == 'D') {
+                        return "ArrowLeft";
+                    }
                 }
             } else {
-                return std::string(1, static_cast<char>(k));
+                return std::string(1, c);
             }
         }
     } catch (...) {
-        struct termios old, newt;
-        tcgetattr(STDIN_FILENO, &old);
-        newt = old;
-        newt.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-        try {
-            for (int i = 0; i < times; ++i) {
-                char c;
-                read(STDIN_FILENO, &c, 1);
-                if (c == 27) {
-                    char seq[2];
-                    read(STDIN_FILENO, seq, 2);
-                    if (seq[0] == '[') {
-                        if (seq[1] == 'A') {
-                            return "ArrowUp";
-                        } else if (seq[1] == 'B') {
-                            return "ArrowDown";
-                        } else if (seq[1] == 'C') {
-                            return "ArrowRight";
-                        } else if (seq[1] == 'D') {
-                            return "ArrowLeft";
-                        } else {
-                            return std::string(1, c) + std::string(seq, 2);
-                        }
-                    }
-                } else {
-                        return std::string(1, c);
-                    }
-                }
-            }
-        } finally {
-            tcsetattr(STDIN_FILENO, TCSANOW, &old);
-        }
-        return "";
+        // Handle any exceptions if necessary
+    } finally {
+        tcsetattr(STDIN_FILENO, TCSANOW, &old);
     }
-
-
+    return result;
+}
 
 PYBIND11_MODULE(geky, m) {
     m.doc() = "Gets a single key input";
