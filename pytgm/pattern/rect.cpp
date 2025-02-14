6aee80d4e3622309e26f92c25fa572a9c5ef4716
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-14 22:36:19 +0000

    Made + started conversion of rect py -> C++

---
+++ pytgm/pattern/rect.cpp
@@ -0,0 +1,24 @@
+#include <pybind11/pybind11.h>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+namespace py = pybind11
+
+void rect(int width, int height, int time=1, const char* char=" ") {
+    int total_steps = (height * (height + 1)) / 2 + (height * (height - 1)) / 2 + (std::max(width - height, 0) * height);
+
+    for (int i = 0; i < height; ++i) {
+        line_length = std::min(++i, width);
+        for (int j = 0; j < line_length) {
+            std::cout << "\x1b[" << (j + 1) << ";" << (i-j+1) << "H" << char;
+        }
+
+    }
+}
+
+PYBIND11_MODULE(rect, m) {
+    m.doc() = "Clears the screen, by printing out a specific char, to make a clearing animation";
+    m.def("geky", &geky, "Prints out a rect of a specified size, in an animated form",
+        py::arg("width"), py::arg("height"), py::arg("time")=1, py::arg("char")=" ");
+}
