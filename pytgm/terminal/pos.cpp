af75c68f52a3629c4d9711a1c2f83e43b4304c39
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-14 03:43:59 +0000

    Made return type auto

--- pytgm/terminal/pos.cpp
+++ pytgm/terminal/pos.cpp
@@ -3,7 +3,7 @@
 #include <Python.h>
 #include <pybind11/pybind11.h>
 
-std::string pos(const int x, const int y) -> std::string {
+auto pos(const int x, const int y) -> std::string {
     std::stringstream ss;
     ss << "\003[" << x << ";" << y << "H";
     return ss.str();
