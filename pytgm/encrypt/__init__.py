4aa9e70eeaa0cb863a4cb4ece3b9c19c05df013b
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-14 17:30:45 +0000

    Changed error to self

--- pytgm/encrypt/__init__.py
+++ pytgm/encrypt/__init__.py
@@ -2,6 +2,6 @@
 Exposes ecryptions
 """
 
-from . import b64, sha256, hk512 #pylint:disable=import-error
+from . import b64, sha256, hk512 #pylint:disable=import-self
 
 __all__ = ['hk512', 'b64', 'sha256']
