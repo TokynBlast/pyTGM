64ca30b4979cd264086e58fb919c575956187e20
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-14 23:01:09 -0800

    Added time in milliseconds

--- pytgm/pattern/rect.cpp
+++ pytgm/pattern/rect.cpp
@@ -2,16 +2,19 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include <threads>
+#include <chrono>
 
 namespace py = pybind11;
 
-void rect(int width, int height, int time=1, const char* character=" ") {
+void rect(int width, int height, int time=100, const char* character=" ") {
     int total_steps = (height * (height + 1)) / 2 + (height * (height - 1)) / 2 + (std::max(width - height, 0) * height);
 
-    for (int i = 0; i < height; ++i) {
+    // for (int i = 0; i < height; ++i) {
         int line_length = std::min(++i, width);
         for (int j = 0; j < line_length; ++j) {
-            std::cout << "\x1b[" << (j + 1) << ";" << (i-j+1) << "H" << char << std::end;
+            std::cout << "\x1b[" << (j + 1) << ";" << (i-j+1) << "H" << character;
+            std::chrono::milliseconds(time)
         }
 
     }
@@ -20,5 +23,5 @@ void rect(int width, int height, int time=1, const char* character=" ") {
 PYBIND11_MODULE(rect, m) {
     m.doc() = "Clears the screen, by printing out a specific char, to make a clearing animation";
     m.def("rect", &rect, "Prints out a rect of a specified size, in an animated form",
-        py::arg("width"), py::arg("height"), py::arg("time")=1, py::arg("character")=" ");
+        py::arg("width"), py::arg("height"), py::arg("time")=100, py::arg("character")=" ");
 }
