93ec263b826aa84e7f6baa57fe2a1cedba5e6945
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-14 23:07:59 -0800

    Uncommented line 13

--- pytgm/pattern/rect.cpp
+++ pytgm/pattern/rect.cpp
@@ -10,7 +10,7 @@ namespace py = pybind11;
 void rect(int width, int height, int time=100, const char* character=" ") {
     int total_steps = (height * (height + 1)) / 2 + (height * (height - 1)) / 2 + (std::max(width - height, 0) * height);
 
-    // for (int i = 0; i < height; ++i) {
+    for (int i = 0; i < height; ++i) {
         int line_length = std::min(++i, width);
         for (int j = 0; j < line_length; ++j) {
             std::cout << "\x1b[" << (j + 1) << ";" << (i-j+1) << "H" << character;
