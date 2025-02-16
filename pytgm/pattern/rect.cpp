9d0ac542a9c6a80caf223ac19748f18b0c45cd39
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-16 11:04:38 -0800

    rect.py -> .cpp

--- pytgm/pattern/rect.cpp
+++ pytgm/pattern/rect.cpp
@@ -7,15 +7,28 @@
 namespace py = pybind11;
 
 void rect(int width, int height, int time=100, const char* character=" ") {
-    int total_steps = (height * (height + 1)) / 2 + (height * (height - 1)) / 2 + (std::max(width - height, 0) * height);
+    int tot_steps = (height * (height + 1)) / 2 + (height * (height - 1)) / 2 + (std::max(width - height, 0) * height);
+    tot_time = time / tot_steps
 
     for (int i = 0; i < height; ++i) {
         int line_length = std::min(++i, width);
         for (int j = 0; j < line_length; ++j) {
             std::cout << "\x1b[" << (j + 1) << ";" << (i-j+1) << "H" << character;
-            std::chrono::milliseconds ms(time);
+            std::chrono::milliseconds ms(tot_time);
         }
+    }
 
+    for (int i = height; i < width; ++i) {
+        for (int j = 0; j < height; ++j) {
+            std::cout << "\x1b[" << (j + 1) << ";" << (i-j+1) << "H" << character;
+            std::chrono::milliseconds ms(tot_time);
+        }
+    }
+
+    for (int i = 0; i < height; ++i) {
+        for (int j = i; j < height) {
+            std::cout << "\x1b[" << (j + 1) << ";" << (width - (j - i)) << "H" << character
+        }
     }
 }
 
