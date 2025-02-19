a0e656a1e115d83356cbdb30f7d6a1079389a053
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-19 12:07:39 -0800

    Changed what's imported on *

--- pytgm/__init__.py
+++ pytgm/__init__.py
@@ -9,21 +9,10 @@ It imports and exposes various modules and functionalities for:
 
 __all__ = [
     'local_server',
-    'server', 'client',
-
     'encrypt',
-    'sha256', 'b64', 'hk512',
-
     'sound',
-    'start', 'stop',
-
     'terminal',
-    'cls', 'color', 'pos',
-    'RESET', 'BOLD', 'UNDERLINE', 'ITALIC',
-    'geky',
-
     'pattern',
-    'rect'
 ]
 
 __url__ = 'https://github.com/TokynBlast/pyTGM'
@@ -31,18 +20,4 @@ __download_url__ = 'https://pypi.org/tokynblast'
 __bug_tracker_url__ = 'https://github.com/TokynBlast/pyTGM/issues'
 __changelog_url__ = 'https://github.com/TokynBlast/pyTGM/blob/main/CHANGELOG.txt'
 
-from . import encrypt, sound, terminal, local_server, pattern
-
-# Sound Imports
-from .sound import start, stop # pylint: disable= import-error, no-name-in-module
-
-# Graphics Imports
-from .terminal import cls, color, pos, RESET, BOLD, ITALIC, UNDERLINE, geky #pylint:disable=import-error
-
-# Online Imports
-from .local_server import server, client
-
-# Ecnrypt Imports
-from .encrypt import sha256, b64, hk512 # pylint:disable=no-name-in-module
-
-from .pattern import rect
+from . import encrypt, sound, terminal, pattern, local_server as server
