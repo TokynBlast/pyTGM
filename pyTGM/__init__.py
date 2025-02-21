d991b0e2df6a220279ad0e0059113ce16b457c42
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-20 18:59:03 -0800

    Changed pytgm to .

--- pyTGM/__init__.py
+++ pyTGM/__init__.py
@@ -23,13 +23,13 @@ __download_url__ = 'https://pypi.org/tokynblast'
 __bug_tracker_url__ = 'https://github.com/TokynBlast/pyTGM/issues'
 __changelog_url__ = 'https://github.com/TokynBlast/pyTGM/blob/main/CHANGELOG.txt'
 
-from pytgm import encrypt, sound, terminal, pattern, local_server as server #pylint: disable=import-self
+from . import encrypt, sound, terminal, pattern, local_server as server #pylint: disable=import-self
 
 # Sound Imports
 from .sound import sound # pylint: disable= import-error
 
 # Graphics Imports
-from .terminal import clear, color, pos, RESET, BOLD, ITALIC, UNDERLINE, geky #pylint:disable=import-error no-name-in-module
+from .terminal import clear, color, pos, RESET, BOLD, ITALIC, UNDERLINE, geky #pylint:disable=import-error
 
 # Online Imports
 from .local_server import server, client
