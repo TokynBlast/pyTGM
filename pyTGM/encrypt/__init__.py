f1e85acb1db6afe9bf7190f7c3396f2362861926
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-21 15:43:29 -0800

    Fixed newline problems

--- pyTGM/encrypt/__init__.py
+++ pyTGM/encrypt/__init__.py
@@ -8,4 +8,3 @@ def __getattr__(name):
     if name in __all__:
         return __import__(f"pyTGM.encrypt.{name}", fromlist=[name])
     raise AttributeError(f"module {__name__} has no attribute {name}")
-
