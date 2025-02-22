7cd600207d9a1ada75071913d9738184db6985c4
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-21 16:45:22 -0800

    Removed lazy loader

--- pyTGM/__init__.py
+++ pyTGM/__init__.py
@@ -22,8 +22,3 @@ __url__ = 'https://github.com/TokynBlast/pyTGM'
 __download_url__ = 'https://pypi.org/tokynblast'
 __bug_tracker_url__ = 'https://github.com/TokynBlast/pyTGM/issues'
 __changelog_url__ = 'https://github.com/TokynBlast/pyTGM/blob/main/CHANGELOG.txt'
-
-def __getattr__(name):
-    if name in __all__:
-        return __import__(f"pyTGM.encrypt.{name}", fromlist=[name])
-    raise AttributeError(f"module {__name__} has no attribute {name}")
