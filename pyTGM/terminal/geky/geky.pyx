4341d860b1c84180e0502485f799fb2e933fc7d4
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-26 10:40:55 -0800

    Changed docstring

--- pyTGM/terminal/geky/geky.pyx
+++ pyTGM/terminal/geky/geky.pyx
@@ -3,13 +3,12 @@
 
 from libcpp.string cimport string as cpp_string
 
-# Declare the external C++ function from the header.
-cdef extern from "geky.h":
+cdef extern from "geky.hpp":
     cpp_string geky(int times=1)
 
 def py_geky(int times=1):
-    """
-    Reads key input(s) and returns a string.
-    """
+    """ Reads key input(s) and returns an RGB string """
     cdef cpp_string s = geky(times)
     return s
+
+geky = py_geky
