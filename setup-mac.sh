f39128feb355eb77b894f406ccb6ed5fe869412f
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-02-26 10:57:04 -0800

    Made setup one file

--- setup-mac.sh
+++
@@ -1,5 +0,0 @@
-# Build pyTGM from source on macOS
-
-brew install cmake python3 pybind11
-
-
---
+++ setup.sh
@@ -0,0 +1,12 @@
+# Build pyTGM
+
+if !command -v python3 >/dev/null; then
+    echo "Python3 is not installed."
+else
+    if !command -v pip list cython wheel setuptools >/dev/null; then
+        echo "One of the following:\n     wheel\n     setuptools\n     cython\nis not installed.\nPython will install it automatically.\n\n"
+    fi
+    echo "Compiling and installing pyTGM..."
+    python3 setup.py sdist
+    python3 -m pip install dist/*
+fi
