0e624ff8ad79f1c9fcc49971ff9c3992dadec08e
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-03-03 19:06:05 -0800

    Added :

--- pyTGM/rect/rect.pyx
+++ pyTGM/rect/rect.pyx
@@ -1,7 +1,7 @@
 # cython: language_level=3, c_string_type=unicode, c_string_encoding=utf8
 # distutils: language = c++
 
-cdef extern from "rect.hpp"
+cdef extern from "rect.hpp":
     void rect(int width, int height, int time=100, const char* character=" ")
 
 def py_rect(int width, int height, int time=100, str character=" ")
