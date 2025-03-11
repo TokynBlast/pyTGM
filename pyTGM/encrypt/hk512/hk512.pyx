7fc4c99b799f040945280baa6c364f3c2abeec2c
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-03-11 09:43:15 -0700

    Fixed hk512 func

--- pyTGM/encrypt/hk512/hk512.pyx
+++ pyTGM/encrypt/hk512/hk512.pyx
@@ -4,17 +4,17 @@
 from libcpp.string cimport string as cpp_string
 
 cdef extern from "hk512.hpp":
-    cpp_string encode_(const cpp_string& input, const cpp_string& key)
-    cpp_string decode_(const cpp_string& data, const cpp_string& key)
+    cpp_string encode_ "encode" (const cpp_string& input, const cpp_string& key)
+    cpp_string decode_ "decode" (const cpp_string& data, const cpp_string& key)
 
-def encode(str input_text, str key):
+cpdef str encode(str input_text, str key):
     """ Encodes the input text using HK512 encryption """
     cdef cpp_string cpp_input = input_text.encode('utf-8')
     cdef cpp_string cpp_key = key.encode('utf-8')
     cdef cpp_string result = encode_(cpp_input, cpp_key)
     return result.decode('utf-8')
 
-def decode(str data, str key):
+cpdef str decode(str data, str key):
     """ Decodes HK512 encrypted text """
     cdef cpp_string cpp_data = data.encode('utf-8')
     cdef cpp_string cpp_key = key.encode('utf-8')
