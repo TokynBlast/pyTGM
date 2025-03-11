0aeaacccc2fe1f1d376e8020f2dd9849452bb0fb
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-03-11 09:17:29 -0700

    Fixed string encoding

--- pyTGM/rect/rect.pyx
+++ pyTGM/rect/rect.pyx
@@ -1,9 +1,14 @@
 # cython: language_level=3, c_string_type=unicode, c_string_encoding=utf8
 # distutils: language = c++
 
+from libcpp.string cimport string as cpp_string
+
 cdef extern from "rect.hpp":
     void rect(int width, int height, int time, const char* character)
 
-def py_rect(int width, int height, int time, str character):
-    """ Prints a rectangle to the sreen """
-    rect(width, height, time, character)
+def py_rect(int width, int height, int time=100, str character=" "):
+    """ Prints a rectangle to the screen """
+    cdef bytes encoded_char = character.encode('utf-8')
+    rect(width, height, time, encoded_char)
+
+rect = py_rect
