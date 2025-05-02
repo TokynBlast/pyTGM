2c5b88e12a8dd3a6f50d4a0b570c1c370e410cbe
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-05-02 08:57:18 -0700

    Changed lazy loader to explicit import

--- pyTGM/__init__.py
+++ pyTGM/__init__.py
@@ -1,13 +1,30 @@
 """ The init file for the pyTGM library """
 
-from .local_server import server, client
+import pyTGM.sound
+import pyTGM.rect
+import pyTGM.terminal
+import pyTGM.encrypt
 
-__all__ = ['server', 'client', 'sound', 'rect', 'terminal', 'encrypt']
+__all__ = [
+    "terminal",
+    "encrypt",
+    "sound",
+    "rect",
+    "RESET",
+    "ITALIC",
+    "BOLD",
+    "UNDERLINE",
+    "INVIS",
+    "BLINK",
+]
 
-def __getattr__(name):
-    if name in __all__:
-        return __import__(f"pyTGM.{name}", fromlist=[name])
-    raise AttributeError(f"module {__name__} has no attribute {name}")
+RESET = "\x1b[0m"
+BOLD = "\x1b[1m"
+ITALIC = "\x1b[3m"
+UNDERLINE = "\x1b[4m"
+INVIS = "\x1b[8m"
+BLINK = "\x1b[5m"
+
 
 __url__ = 'https://github.com/TokynBlast/pyTGM'
 __download_url__ = 'https://pypi.org/tokynblast'
