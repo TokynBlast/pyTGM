dcb2e2f9648c164ff2f75f0e95fc8a4f23768a35
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-05-09 11:46:33 -0700

    Added aliases

--- pyTGM/__init__.py
+++ pyTGM/__init__.py
@@ -1,10 +1,12 @@
-""" The init file for the pyTGM library """
-from . import sound
-from . import rect
-__all__ = [
-    "sound",
-    "rect",
-]
+"""The init file for the pyTGM library"""
+from . import sound as _sound
+from . import rect as _rect
+
+__all__ = ["sound", "rect"]
+
+# Public aliases
+sound = _sound
+rect = _rect
 
 __url__ = 'https://github.com/TokynBlast/pyTGM'
 __download_url__ = 'https://pypi.org/tokynblast'
