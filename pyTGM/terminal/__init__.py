2cae49fcfc82afff1692243cc69c516fc16a3ab8
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-05-09 11:56:03 -0700

    Added importlib

--- pyTGM/terminal/__init__.py
+++ pyTGM/terminal/__init__.py
@@ -1,6 +1,17 @@
 """Everything that directly affects the terminal visuals"""
+import importlib
 
-from . import geky, clear, color, pos
+geky = importlib.import_module('.geky', __name__)
+clear = importlib.import_module('.clear', __name__)
+color = importlib.import_module('.color', __name__)
+pos = importlib.import_module('.pos', __name__)
+
+RESET = "\x1b[0m"
+BOLD = "\x1b[1m"
+ITALIC = "\x1b[3m"
+UNDERLINE = "\x1b[4m"
+INVIS = "\x1b[8m"
+BLINK = "\x1b[5m"
 
 __all__ = [
     "geky",
@@ -14,10 +25,3 @@ __all__ = [
     "INVIS",
     "BLINK",
 ]
-
-RESET = "\x1b[0m"
-BOLD = "\x1b[1m"
-ITALIC = "\x1b[3m"
-UNDERLINE = "\x1b[4m"
-INVIS = "\x1b[8m"
-BLINK = "\x1b[5m"
