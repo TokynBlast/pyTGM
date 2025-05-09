c8b0c04448a08081abeb9ec91ab6fc670abbf375
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-05-09 11:57:38 -0700

    Moved sha256 import down

--- pyTGM/encrypt/__init__.py
+++ pyTGM/encrypt/__init__.py
@@ -1,6 +1,6 @@
 """Init file for pyTGM.encrypt"""
-from . import sha256
 import importlib
+from . import sha256
 
 b64 = importlib.import_module('.b64', __name__)
 hk512 = importlib.import_module('.hk512', __name__)
