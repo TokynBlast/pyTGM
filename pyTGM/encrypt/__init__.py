8d6571deb2cd722ad44ae5a4b3174cc1fa01549a
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-05-09 11:34:23 -0700

    Fixed encrypt imports
    Made it explicit instead of lazy

--- pyTGM/encrypt/__init__.py
+++ pyTGM/encrypt/__init__.py
@@ -1,11 +1,6 @@
-"""
-Exposes ecryptions
-"""
+"""Init file for pyTGM.encrypt"""
+from . import hk512
+from . import b64
+from . import sha256
 
 __all__ = ['hk512', 'b64', 'sha256']
-
-
-def __getattr__(name):
-    if name in __all__:
-        return __import__(f"pyTGM.encrypt.{name}", fromlist=[name])
-    raise AttributeError(f"module {__name__} has no attribute {name}")
