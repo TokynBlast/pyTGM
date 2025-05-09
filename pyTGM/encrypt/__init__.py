4975780ab5e20b3ed1a7ffda190c3bcd43f5e4ca
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-05-09 11:52:38 -0700

    Added importlib

--- pyTGM/encrypt/__init__.py
+++ pyTGM/encrypt/__init__.py
@@ -1,6 +1,9 @@
 """Init file for pyTGM.encrypt"""
-from . import hk512
-from . import b64
+import importlib
+
+b64 = importlib.import_module('.b64', __name__)
+hk512 = importlib.import_module('.hk512', __name__)
+
 from . import sha256
 
 __all__ = ['hk512', 'b64', 'sha256']
