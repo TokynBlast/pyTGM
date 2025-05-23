421dafb35be3aff8bdf1ca0832cd1df9b8be8688
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-05-23 09:16:14 -0700

    ദ്ദി •⩊• )   Shortened doc-string

--- pyTGM/__init__.py
+++ pyTGM/__init__.py
@@ -1,4 +1,4 @@
-"""The init file for the pyTGM library""" #pylint:disable=invalid-name
+"""The initiation file""" #pylint:disable=invalid-name
 import importlib
 
 sound = importlib.import_module('.sound', __name__)
