75cb1614d65eb7dd90af7ae41a566de4c28bffac
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-06-02 09:27:40 -0700

    Fixed sound and finished nanobind

--- pyTGM/sound.cpp
+++ pyTGM/sound.cpp
@@ -15,7 +15,9 @@
 
 #ifdef __linux__
     #define linux_
-    #include <cstdlib>
+    #include <alsa/asoundlib.h>
+    #include <fstream>
+    #include <vector>
 #endif
 
 #include <iostream>
@@ -23,7 +25,7 @@
 
 int sound(const char* filename) {
     #if defined(WIN)
-        PlaySound(TEXT(filename), NULL, SND_FILENAME | SND_ASYNC);
+        PlaySoundA(filename, NULL, SND_FILENAME | SND_ASYNC);
 
     #elif defined(__APPLE__)
         CFStringRef cfString = CFStringCreateWithCString(NULL, filename, kCFStringEncodingUTF8);
@@ -35,13 +37,56 @@ int sound(const char* filename) {
         CFRelease(cfString);
 
     #elif defined(__linux__)
-        std::string command = "aplay -q ";
-        command += filename;
-        command += " &";
-        std::system(command.c_str());
+        snd_pcm_t* handle;
+        snd_pcm_hw_params_t* params;
+        unsigned int rate = 44100;
+        int channels = 2;
+        snd_pcm_uframes_t frames = 32;
+
+        if (snd_pcm_open(&handle, "default", SND_PCM_STREAM_PLAYBACK, 0) < 0) {
+            std::cerr << "Error opening PCM device." << std::endl;
+            return 1;
+        }
+
+        snd_pcm_hw_params_malloc(&params);
+        snd_pcm_hw_params_any(handle, params);
+        snd_pcm_hw_params_set_access(handle, params, SND_PCM_ACCESS_RW_INTERLEAVED);
+        snd_pcm_hw_params_set_format(handle, params, SND_PCM_FORMAT_S16_LE);
+        snd_pcm_hw_params_set_channels(handle, params, channels);
+        snd_pcm_hw_params_set_rate_near(handle, params, &rate, nullptr);
+        snd_pcm_hw_params_set_period_size_near(handle, params, &frames, nullptr);
+        snd_pcm_hw_params(handle, params);
+        snd_pcm_hw_params_free(params);
+
+        std::ifstream file(filename, std::ios::binary);
+        if (!file) {
+            std::cerr << "Could not open file.\n";
+            return 1;
+        }
+
+        file.seekg(44); // Skip WAV header
+        std::vector<char> buffer(4096);
+        while (!file.eof()) {
+            file.read(buffer.data(), buffer.size());
+            auto size = file.gcount();
+            if (size > 0)
+                snd_pcm_writei(handle, buffer.data(), size / 4); // 16-bit stereo
+        }
+
+        snd_pcm_drain(handle);
+        snd_pcm_close(handle);
+
     #else
         std::cerr << "Sound playback not supported on this platform." << std::endl;
         return 1;
     #endif
+
     return 0;
-};
+}
+
+namespace nb = nanobind;
+using namespace nb::literals;
+
+NB_MODULE(pyTGM, m) {
+    m.def("sound", &sound, "filename"_a, "Play a WAV sound file.");
+}
